959ec0db367208e4c3f75e6bb8f6d4de5de801a4
daidh1 <daidh@goline.vn>
2022-09-29 19:14:45 +0700

    demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleListComponent } from './example-list/example-list.component';
+import { FaceDetectionComponent } from './face-detection/face-detection.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'face-detection', component: FaceDetectionComponent },
+  { path: 'example-list', component: ExampleListComponent },
+  { path: '', redirectTo: '/example-list', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  declarations: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgOpenCVModule, OpenCVOptions } from 'ng-open-cv';
+import { FaceDetectionComponent } from './face-detection/face-detection.component';
+import { ExampleListComponent } from './example-list/example-list.component';
+import { RouterModule } from '@angular/router';
+
+const openCVConfig: OpenCVOptions = {
+  scriptUrl: `assets/opencv/opencv.js`,
+  wasmBinaryFile: 'wasm/opencv_js.wasm',
+  usingWasm: true
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceDetectionComponent,
+    ExampleListComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOpenCVModule.forRoot(openCVConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
